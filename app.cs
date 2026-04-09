c3972b6fad327bfd45163147a9ad199c126b7431
Nekrudzo <130899u@mail.ru>
2026-04-09 18:48:13 +0500

    new feature

--- app.cs
+++ app.cs
@@ -8,11 +8,31 @@ using System;
         static int Multiply(int a, int b){
         return a * b;
     }
+    
+        static int Add(int a, int b){
+        return a + b;
+    }
        static void Main() {
-           Subtract(30, 12)
-           Multiply(2, 4)
-           Console.WriteLine("Калькулятор v2.0-FINAL");
+        Console.Write("Введите первое число: ");
+        string input1 = Console.ReadLine();
+        int num1 = int.Parse(input1);
+    
+        Console.Write("Введите второе число: ");
+        string input2 = Console.ReadLine();
+        int num2 = int.Parse(input2);
+    
+        Console.Write("Выберите операцию (+, -, *): ");
+        string operation = Console.ReadLine();
+    
+        int result = 0;
+    
+        if (operation == "+")
+            result = num1 + num2;
+        else if (operation == "-")
+            result = num1 - num2;
+        else if (operation == "*")
+            result = num1 * num2;
+    
+        Console.WriteLine($"Результат: {result}");
        }
-       Console.WriteLine($"Результат: {Subtract}");
-       Console.WriteLine($"Результат: {Multiply}");
    }
