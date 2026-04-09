ed72376e394ba192f4eab42e5c2441b4a08d882f
Nekrudzo <130899u@mail.ru>
2026-04-09 18:09:04 +0500

    Добавлен метод subtract

--- app.cs
+++ app.cs
@@ -1,6 +1,11 @@
 using System;
    class App {
+    static int Subtract(int a, int b){
+        return a - b;
+    }
        static void Main() {
+           Subtract(30, 12)
            Console.WriteLine("Калькулятор");
        }
+       Console.WriteLine($"Результат: {Subtract}");
    }
